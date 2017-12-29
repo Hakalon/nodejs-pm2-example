f7a105ecb54956f9b59304e659f2101538c07a4a
Steve Lai <tf4ewg@gmail.com>
2017-12-29 16:19:21 +0800

    install pm2 & loadtest

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ app.get('/', (req, res) => {
   }, 300)
   console.log(`root had been called.`)
 })
-
+console.log(`Starting listen port 3000`);
 app.listen(3000)
+console.log(`Successfully listening port 3000`);
